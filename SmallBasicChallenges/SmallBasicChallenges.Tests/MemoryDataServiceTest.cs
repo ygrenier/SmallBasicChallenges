ce897e03aed099805a03a6ea28ab939efd3859d8
Yan Grenier <ygrenier@ygrenier.com>
2015-03-20 07:16:51 +0100

    Prepare tests for MemoryDataService.

---
+++ SmallBasicChallenges/SmallBasicChallenges.Tests/MemoryDataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmallBasicChallenges.Tests
+{
+    public class MemoryDataServiceTest
+    {
+        [Fact]
+        public void TestCreate()
+        {
+            var ds = new MemoryDataService();
+            
+        }
+
+    }
+}
